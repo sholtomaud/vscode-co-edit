e621a44c03ee5c455250e2f15f6409d736a03577
Sholto Maud <sholto.maud@gmail.com>
2025-06-28 11:06:25 +1000

    feat: Implement and register draftChapterPlan command (closes #5)

--- src/extension.ts
+++ src/extension.ts
@@ -13,10 +13,9 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-co-edit.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('co-pilot.draftChapterPlan', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-co-edit!');
+		vscode.window.showInformationMessage('Draft Chapter Plan command executed!');
 	});
 
 	context.subscriptions.push(disposable);
