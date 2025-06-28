8be1cee24475739f7b8296dac0b6f4de05916339
Sholto Maud <sholto.maud@gmail.com>
2025-06-28 11:10:41 +1000

    feat: Write generated plan to file (closes #8)

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as path from 'path';
+import { exec } from 'child_process';
+import * as fs from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
